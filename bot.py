077036777090bff795801cd86092b2645ead9b7a
proguy914629 <gilbert@pt-gss.com>
2021-10-12 17:13:26 +0700

    Fix??

--- bot.py
+++ bot.py
@@ -176,7 +176,7 @@ async def lyrics(ctx, *, query: str):
 
             await asyncio.sleep(3)
 
-            for act in bot.get_user(ctx.author.id).activities:
+            for act in ctx.guild.get_member(ctx.author.id).activities:
                 if isinstance(act, discord.Spotify):
                     activity = act
                     break
