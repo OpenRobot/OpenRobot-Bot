c4551720feb073958b1ccd09d764b451ff528023
proguy914629 <gilbert@pt-gss.com>
2021-11-19 12:48:12 +0700

    Fix

--- cogs/jsk.py
+++ cogs/jsk.py
@@ -292,7 +292,7 @@ Ping:
 - Jitter: {round(data['ping']['jitter'], 2)} ms
 - Latency: {round(data['ping']['latency'], 2)} ms
 
-Packet Loss: {round(data['packetLoss'], 2) + '%' if 'packetLoss' not in data else 'Not available.'}
+Packet Loss: {round(data['packetLoss'], 2) + '%' if 'packetLoss' in data else 'Not available.'}
 ```Result URL: {data['result']['url']}
                 """, inline=False)
 
