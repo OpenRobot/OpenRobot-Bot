523de7832f4d31341ddf917dc9d2ff8e6c839d23
proguy914629 <gilbert@pt-gss.com>
2021-11-24 22:21:05 +0700

    Fix

--- bot.py
+++ bot.py
@@ -761,7 +761,7 @@ async def spotify(ctx: commands.Context, *, member: discord.Member = None):
     artists = []
 
     try:
-        async with bot.session.get('https://accounts.spotify.com/api/token', params={'grant_type': 'client_credentials'}, headers={'Authorization': f'Basic {base64.urlsafe_b64encode(f"{bot.spotify._client_id}:{bot.spotify._client_secret}".encode()).decode()}', 'Content-Type': 'application/x-www-form-urlencoded'}) as resp:
+        async with bot.session.post('https://accounts.spotify.com/api/token', params={'grant_type': 'client_credentials'}, headers={'Authorization': f'Basic {base64.urlsafe_b64encode(f"{bot.spotify._client_id}:{bot.spotify._client_secret}".encode()).decode()}', 'Content-Type': 'application/x-www-form-urlencoded'}) as resp:
             js = await resp.json()
     except Exception as e:
         if ctx.debug:
