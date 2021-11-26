6a2a25a3ea27eece3b3353d332719292ecbb619b
proguy914629 <gilbert@pt-gss.com>
2021-11-26 22:09:43 +0700

    Fix

--- cogs/jsk.py
+++ cogs/jsk.py
@@ -362,8 +362,8 @@ Packet Loss: {str(round(data['packetLoss'], 2)) + '%' if 'packetLoss' in data el
 Exited with code {proc.returncode}.```
         """
 
-        await ctx.send(stdout.decode())
-        await ctx.send(stderr.decode())
+        await ctx.send(stdout.decode() or 'Empty.')
+        await ctx.send(stderr.decode() or 'Empty.')
 
         embed.set_author(name='Sync', icon_url=ctx.author.avatar.url)
 
