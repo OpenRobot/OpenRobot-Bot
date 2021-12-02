8c3566c02dc48312023b9db442f72c3a1a33913e
proguy914629 <gilbert@pt-gss.com>
2021-12-02 09:18:02 +0700

    CPU Name in jsk sys command

--- cogs/jsk.py
+++ cogs/jsk.py
@@ -8,6 +8,7 @@ import platform
 import speedtest
 import asyncio
 import re
+import cpuinfo
 from discord.ext import commands
 from humanize import naturalsize as get_size
 from jishaku.features.baseclass import Feature
@@ -220,6 +221,7 @@ Processor: {processor}```
             embed.add_field(
                 name="CPU:",
                 value=f"""```yml
+Name: {cpuinfo.get_cpu_info()['raw_brand']}
 Physical cores: {physical_cores}
 Total cores: {total_cores}
 Frequency: {current_cpu_freq}
