ab6d8caa0bdda4743ab02826341e4d449e633607
proguy914629 <gilbert@pt-gss.com>
2021-12-15 11:04:20 +0700

    Fix

--- cogs/fun.py
+++ cogs/fun.py
@@ -415,7 +415,7 @@ class Fun(Cog, emoji=""):  # TODO: Put fun emoji
                 embed.description = f"""
 Guessed Words: {', '.join([f'`{x}`' for x in hangman.guessed_words])}
 Word: {hangman.word_guess}
-Tries Left: {hangman.tries_left}
+Tries Left: {hangman.tries}
 ```{hangman.draw()}```
 
 Please reply to this message with a letter.
@@ -438,6 +438,9 @@ Please reply to this message with a letter.
 
                 await msg.delete()
             except Exception as e:
+                if not isinstance(e, games.HangmanExcpetion):
+                    return await ctx.send(f"```py\n{e}```")
+                    
                 await ctx.send(f"```py\n{e}```")
 
                 if msg:
