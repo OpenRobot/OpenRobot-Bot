5529fb270fdf2e148ba732f32c3759e207a6f0a9
proguy914629 <proguy914629@openrobot.xyz>
2022-04-25 13:30:36 +0700

    Debug?

--- cogs/utils/spotify/__init__.py
+++ cogs/utils/spotify/__init__.py
@@ -140,6 +140,8 @@ def _spotify(title, artists, cover_buff, duration, start, *, beta = False):
 
         end_pos = int(((dt.datetime.now() - dt.datetime.fromtimestamp(start)).seconds / duration) * 450)
 
+        print(end_pos)
+
         a, b = 431, 881
 
         while b-a > end_pos:
@@ -148,6 +150,8 @@ def _spotify(title, artists, cover_buff, duration, start, *, beta = False):
 
         if a+b != end_pos:
             a += 1
+
+        print(a, b)
 
         bar = Image.new('RGBA', (end_pos, 15), fcolor)
         img.paste(bar, (a, 285, b, 300), bar)
