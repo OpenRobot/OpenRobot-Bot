7dab3e93088e2bb643d9aa2b7fe0d400fcae991f
proguy914629 <proguy914629@openrobot.xyz>
2022-06-21 19:58:38 +0700

    Remove logging

--- bot.py
+++ bot.py
@@ -254,11 +254,11 @@ def override(func):  # Plainly just for `source` command.
     return func
 
 
-logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
+# logger = logging.getLogger('discord')
+# logger.setLevel(logging.DEBUG)
+# handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+# handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+# logger.addHandler(handler)
 
 
 @bot.event
